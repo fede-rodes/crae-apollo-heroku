43dc8e722e6eb1949ec4343d205bcfeedb7edc4e
fede-rodes <rodesf@gmail.com>
2017-07-28 01:00:39 +0200

    starting with mongoDB

--- server/src/connectors.js
+++ server/src/connectors.js
@@ -1,3 +1,51 @@
+import mongoose from 'mongoose';
+import casual from 'casual';
+import _ from 'lodash';
+
+const MONGO_DB = process.env.MONGO_DB;
+mongoose.connect(MONGO_DB);
+mongoose.Promise = global.Promise;
+console.log('MONGO_DB', MONGO_DB);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('DB is connected!');
+});
+
+const authorSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+});
+
+const postSchema = mongoose.Schema({
+  authorId: String,
+  title: String,
+  text: String,
+});
+
+const Author = mongoose.model('Author', authorSchema);
+const Post = mongoose.model('Post', postSchema);
+
+casual.seed(11);
+// db.sync({ force: true }).then(() => {
+  _.times(1, () => (
+    new Author({
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+    }).save().then(author => (
+      new Post({
+        authorId: author._id, // eslint-disable-line
+        title: `A post by ${author.firstName}`,
+        text: casual.sentences(3),
+      }).save()
+    ))
+  ));
+// });
+
+export { Author, Post };
+
+/*
 import Sequelize from 'sequelize';
 import casual from 'casual';
 import _ from 'lodash';
@@ -39,3 +87,4 @@ const Author = db.models.author;
 const Post = db.models.post;
 
 export { Author, Post };
+*/
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,6 +3,46 @@ import { Author, Post } from './connectors';
 
 const resolvers = {
   Query: {
+    authors: () => (Author.find({})),
+    author: (root, args) => {
+      const query = {};
+      _.each(['firstName', 'lastName'], (fieldName) => {
+        if (args[fieldName] && args[fieldName].trim().length > 0) {
+          _.extend(query, { [fieldName]: args[fieldName].trim() });
+        }
+      });
+
+      if (_.isEmpty(query)) {
+        return null;
+      }
+      console.log('query', query);
+      return Author.findOne({ query });
+    },
+  },
+  // We need to tell graphql how to resolve the 'posts' field inside the Author
+  // query. We could also define resolver functions for the rest of the Author
+  // fields, but if we don't do that graphql will default to the field values,
+  // which is exactly what we want.
+  Author: {
+    posts(author) {
+      return Post.find({ authorId: author._id }); // author.getPosts();
+    },
+  },
+  Post: {
+    author(post) {
+      return Author.findOne({ _id: post.authorId }); // post.getAuthor();
+    },
+  },
+};
+
+export default resolvers;
+
+/*
+import _ from 'lodash';
+import { Author, Post } from './connectors';
+
+const resolvers = {
+  Query: {
     authors: () => (Author.findAll({})),
     author: (root, args) => {
       const query = {};
@@ -36,3 +76,4 @@ const resolvers = {
 };
 
 export default resolvers;
+*/
