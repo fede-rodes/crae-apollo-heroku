7b96a0c8375ebcad3115b3cb628f68924cd2edf8
federodes <federodes@gmail.com>
2018-11-27 12:51:08 +0100

    removing logs

--- server/src/graphql/user/resolvers/mutation/login.js
+++ server/src/graphql/user/resolvers/mutation/login.js
@@ -32,7 +32,6 @@ const login = async (root, args) => {
 
   const token = user.genAuthToken();
 
-  console.log('LOGIN RESPONSE', { _id: user._id, token });
   // Successful request
   return { _id: user._id, token };
 };
