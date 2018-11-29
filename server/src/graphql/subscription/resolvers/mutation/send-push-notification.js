fede7500ba935d0abb3236f25e44513f4650271d
federodes <federodes@gmail.com>
2018-11-29 12:09:53 +0100

    cleaning server  push notif code

--- server/src/graphql/subscription/resolvers/mutation/send-push-notification.js
+++ server/src/graphql/subscription/resolvers/mutation/send-push-notification.js
@@ -11,7 +11,7 @@ const asyncForEach = require('../../../../utils/async-for-each');
 * @summary Send push notification to all subscribed users.
 */
 const sendPushNotification = async (root, args, context) => {
-  // const { title, body, icon } = args;
+  const { title = 'Hey!', body = 'This is a push notification' } = args; // TODO: add (default) icon
   const { usr } = context;
   console.log('\nSend push notification');
 
@@ -23,18 +23,18 @@ const sendPushNotification = async (root, args, context) => {
   console.log('\nsubs', subs);
 
   // Send the messages
-  asyncForEach(subs, async (subscription) => {
+  asyncForEach(subs, async (sub) => {
     try {
       await pushAPI.send({
-        subscription: pick(subscription, ['endpoint', 'keys']),
-        title: 'Hey',
-        body: 'This is a push notification!',
+        subscription: pick(sub, ['endpoint', 'keys']),
+        title,
+        body,
         // icon,
       });
     } catch (exc) {
       console.log(exc);
       // This is probably an old subscription, remove it
-      await Subscription.deleteOne({ userId: usr._id, endpoint: subscription.endpoint }).exec();
+      await Subscription.deleteOne({ userId: usr._id, endpoint: sub.endpoint }).exec();
     }
   });
 
@@ -43,63 +43,3 @@ const sendPushNotification = async (root, args, context) => {
 //------------------------------------------------------------------------------
 
 module.exports = sendPushNotification;
-
-
-/*
-const pick = require('lodash/pick');
-const { Subscription } = require('../../../../models');
-const pushAPI = require('../../../../services/push');
-const asyncForEach = require('../../../../utils/async-for-each');
-// import utils from '../../utils';
-
-//------------------------------------------------------------------------------
-// MUTATION:
-//------------------------------------------------------------------------------
-/**
-* @summary Send push notification to all subscribed users.
-/
-const sendPushNotification = async (root, args, context) => {
-  // const { title, body, icon } = args;
-  const { usr } = context;
-  console.log('\nSend push notification');
-
-  // TODO: use middleware
-  // Users.utils.checkLoggedInAndVerified(userId);
-
-  // Gather all subscriptions from all subscribed users
-  let subscriptions = [];
-  try {
-    subscriptions = await Subscription.find({}).select({ endpoint: 1, keys: 1 }).exec();
-  } catch (exc) {
-    return { status: 500 }; // TODO: throw
-  }
-  console.log('\nsubscriptions', subscriptions);
-
-  // Send the messages
-  asyncForEach(subscriptions, async (subscription) => {
-    let response;
-    try {
-      response = await pushAPI.send({
-        subscription: pick(subscription, ['endpoint', 'keys']),
-        title: 'Welcome',
-        body: 'Thank you for enabling push notifications',
-        // icon,
-      });
-    } catch (exc) {
-      console.log(exc); // TODO: throw
-    }
-
-    // TODO: move this code to the catch block
-    if (response && response.error) {
-      // This is probably an old subscription, remove it
-      await Subscription.deleteOne({ userId: usr._id, endpoint: subscription.endpoint });
-    }
-  });
-
-  return { status: 200 };
-};
-//------------------------------------------------------------------------------
-
-module.exports = sendPushNotification;
-
-*/
