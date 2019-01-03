555b5b654a3f639cb069c954f1e3a6a01118d683
federodes <federodes@gmail.com>
2019-01-03 17:44:58 +0100

    tests for loginApiCall

--- client/src/graphql/mocks.js
+++ client/src/graphql/mocks.js
@@ -1,6 +1,11 @@
 const resolvers = {
   Mutation: {
-    login: () => ({ _id: '123', token: 'xyz123' }),
+    login: (parent, { email, passcode }) => {
+      if (email && email === 'email@example.com' && passcode && passcode === 123456) {
+        return { _id: '123', token: 'xyz123' };
+      }
+      throw new Error();
+    },
   },
 };
 
