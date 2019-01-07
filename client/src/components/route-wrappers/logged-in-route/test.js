53629d26fdd6e6c21ab18a798b07699c589a91c3
federodes <federodes@gmail.com>
2019-01-07 16:02:53 +0100

    tests

--- client/src/components/route-wrappers/logged-in-route/test.js
+++ client/src/components/route-wrappers/logged-in-route/test.js
@@ -10,26 +10,24 @@ import LoggedInRoute from '.';
 
 const renderRoute = (path, context) => mount(
   <MemoryRouter initialEntries={[path]}>
-    <ApolloProvider client={mockClient}>
-      <Switch>
-        <LoggedInRoute
-          path="/"
-          component={() => (<div><h1>Home</h1></div>)}
-          authComponent={() => (<div><h1>Log in</h1></div>)}
-        />
-      </Switch>
-    </ApolloProvider>
+    <Switch>
+      <LoggedInRoute
+        path="/"
+        component={() => (<div><h1>Home</h1></div>)}
+        authComponent={() => (<div><h1>Log in</h1></div>)}
+      />
+    </Switch>
   </MemoryRouter>,
   context,
 );
 
 describe('LoggedInRoute', () => {
-  // it('renders without crashing', () => {
-  //   const div = document.createElement('div');
-  //   const wrapper = renderRoute('/', {});
-  //   ReactDOM.render(wrapper, div);
-  //   // wrapper.unmount();
-  // });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    const wrapper = renderRoute('/', {});
+    ReactDOM.render(wrapper, div);
+    wrapper.unmount();
+  });
 
   // it('renders Log in text if user is NOT authenticated when visiting /', () => {
   //   const context = { curUser: null };
@@ -47,19 +45,26 @@ describe('LoggedInRoute', () => {
   //   // wrapper.unmount();
   // });
 
-  it('renders Home text if user IS authenticated when visiting /', () => {
-    const context = {
-      curUser: {
-        _id: '123',
-        createdAt: new Date(),
-        email: 'example@email.com',
-      },
-    };
-    const wrapper = renderRoute('/', { context });
-    console.log(wrapper.find({ prop: 'path' }).debug());
-    // console.log(wrapper.find('Component').debug());
-    expect(wrapper.find({ prop: 'path' }).dive().find('h1').text()).toBe('Log in');
-    // expect(wrapper.find('h1').text()).toBe('Log In');
-    // wrapper.unmount();
-  });
+  // it('renders Home text if user IS authenticated when visiting /', () => {
+  //   const context = {
+  //     client: {
+  //       query: {
+  //         user: {
+  //           data: {
+  //             _id: '123',
+  //             createdAt: new Date(),
+  //             email: 'example@email.com',
+  //           }
+  //         },
+  //       }
+  //     },
+  //   };
+  //   const wrapper = renderRoute('/', { options: { context } });
+  //   console.log(wrapper.debug());
+  //   // console.log(wrapper.find({ prop: 'path' }).debug());
+  //   // console.log(wrapper.find('Component').debug());
+  //   expect(wrapper.find({ prop: 'path' }).dive().find('h1').text()).toBe('Log in');
+  //   // expect(wrapper.find('h1').text()).toBe('Log In');
+  //   // wrapper.unmount();
+  // });
 });
